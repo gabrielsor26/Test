84edb838c1bf91f4960652bcfd5d25faadd4e384
gabrielsor26 <147194464+gabrielsor26@users.noreply.github.com>
2024-11-11 21:41:07 -0500

    Update Test.java

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -2,8 +2,8 @@
 package com.example.test;
 //GABRIELSOR_ESCRITODESDE GIT
 public class Test {
-//Holi
+//Holi + como estas desde git
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! + desde git");
     }
 }
