40e8eb3e029ec0fd4691ca23a4bb6de0af8f67a2
gabrielsor26 <gabriel.vilchez.chota@gmail.com>
2024-11-11 21:43:13 -0500

    Commit desde neatbeanss ahora nooooooo

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -2,8 +2,8 @@
 package com.example.test;
 //GABRIELSOR_ESCRITODESDE GIT
 public class Test {
-//Holi + como estas desde git ahora siiiii
+//Holi + como estas desde git ahora noooooooooooooooooooo
     public static void main(String[] args) {
-        System.out.println("Hello World! + desde git ahora siiiii");
+        System.out.println("Hello World! + desde git ahora nooooooooooooooo");
     }
 }
